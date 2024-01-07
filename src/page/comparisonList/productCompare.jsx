90df49687a58e496b790d7b122e4e948035f1afd
AlexGritsuk <alexey1212@inbox.ru>
2024-01-07 13:31:15 +0300

    before the change IphonePage

--- src/page/comparisonList/productCompare.jsx
+++ src/page/comparisonList/productCompare.jsx
@@ -1,20 +1,20 @@
 import React from "react";
-import style from "./iphoneCompare.module.css";
+import style from "./productCompare.module.css";
 
 const ProductCompare = ({ ...compareIphone }) => {
   return (
-    <div className={style.iphoneCompare__wrapper}>
-      <div className={style.iphoneCompare}> 
-        <div className={style.iphoneCompare__img}>
+    <div className={style.productCompare__wrapper}>
+      <div className={style.productCompare}> 
+        <div className={style.productCompare__img}>
           <img  src={compareIphone.img[0]} alt="" />
         </div>
 
-        <div className={style.iphoneCompare__description}>
-          <div className={style.iphoneCompare__description__item}>
+        <div className={style.productCompare__description}>
+          <div className={style.productCompare__description__item}>
             <div>{compareIphone.name.name}</div>
             {/* <div>{compareIphone.memories.memory}</div> */}
           </div>
-          <div className={style.iphoneCompare__description__item}>
+          <div className={style.productCompare__description__item}>
             {compareIphone.visualAppearance.name}
           </div>
         </div>
